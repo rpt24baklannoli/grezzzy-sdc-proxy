d8df314d4cf08e4d2f008135f4cd868da850220f
Gabriel Gutierrez <gabe.grez@gmail.com>
2021-03-09 18:32:08 -0800

    Remove console logs

--- server/index.js
+++ server/index.js
@@ -5,10 +5,10 @@ const axios = require('axios');
 const cors = require('cors');
 const app = express();
 const port = 3000;
-const imagesIP = 'localhost:3006';
-const shoppingIP = '18.222.223.190:3004';
-const reviewsIP = '54.151.123.24:3002';
-const sellerIP = '3.21.248.149:3005';
+const shoppingIP = '13.52.16.25:3004';
+const sellerIP = '18.221.203.49:3005';
+const reviewsIP = '54.176.185.40:3002';
+const imagesIP = '3.101.55.156:3006';
 
 app.use(compression());
 app.use(express.json());
@@ -18,13 +18,12 @@ app.use('/items/:itemId', express.static('client'));
 
 app.get('/serviceBundles', (req, res) => {
 	let images = axios.get(`http://${imagesIP}/items/1/bundle.js`);
-	// let shopping = axios.get(`http://${shoppingIP}/items/1/bundle.js`);
-	// let reviews = axios.get(`http://${reviewsIP}/items/1/bundle.js`);
-	// let seller = axios.get(`http://${sellerIP}/items/1/bundle.js`);
-
+	let shopping = axios.get(`http://${shoppingIP}/items/1/bundle.js`);
+	let reviews = axios.get(`http://${reviewsIP}/items/1/bundle.js`);
+	let seller = axios.get(`http://${sellerIP}/items/1/bundle.js`);
 	// ****add images service bundle when ready to promise
-	//Promise.all([images, shopping, reviews, seller])
-	Promise.all([images])
+	Promise.all([images, shopping, reviews, seller])
+	// Promise.all([images])
 		.then((response) => {
 			let data = '';
 			response.forEach((resp) => {
@@ -118,16 +117,16 @@ app.get('/shopping/items', (req, res) => {
 		});
 });
 
-app.get('/item/images/distinct', (req, res) => {
-	axios
-		.get(`http://${imagesIP}/item/images/distinct`)
-		.then((response) => {
-			res.status(200).send(response.data);
-		})
-		.catch((err) => {
-			res.status('404');
-		});
-});
+// app.get('/item/images/distinct', (req, res) => {
+// 	axios
+// 		.get(`http://${imagesIP}/item/images/distinct`)
+// 		.then((response) => {
+// 			res.status(200).send(response.data);
+// 		})
+// 		.catch((err) => {
+// 			res.status('404');
+// 		});
+// });
 
 app.listen(port, () => {
 	// eslint-disable-next-line no-console
